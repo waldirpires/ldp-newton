3e89719531484fe82ecbf6fdd7136e0d070985a6
Waldir R. Pires Jr <w.pires.ext@planerio.de>
2021-10-29 08:24:46 -0300

    28/10: Ex2: comando GREP

--- 2021-2-ldp/src/edu/newton/ldp/u6/io/chars/Ex2PesquisaArqTexto.java
+++ 2021-2-ldp/src/edu/newton/ldp/u6/io/chars/Ex2PesquisaArqTexto.java
@@ -1,11 +1,44 @@
 package edu.newton.ldp.u6.io.chars;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Ex2PesquisaArqTexto {
 
-	// Problema:
-	// Entrada: nome de um arquivo e uma palavra
+	// Problema (grep):
+	// Entrada: nome de um arquivo texto e uma palavra lidos do usuário
+		// JOptionPane ou Scanner(System.in)
 	// Saída: informar se a palavra existe no arquivo ou não
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Digite o nome do arquivo: ");
+		var arq = sc.nextLine();
+		System.out.println("Digite a palavra: ");
+		var palavra = sc.next();
+		
+		BufferedReader br = null;
+		
+		try {
+			br = new BufferedReader(new FileReader(arq));
+			
+			String linha = null;
+			while ((linha = br.readLine()) != null) {
+				if (linha.contains(palavra)) {
+					System.out.println("INFO: palavra encontrada no arquivo!");
+				}
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		} finally {
+			if (br != null) {
+				br.close();
+			}
+		}
+		sc.close();
 	}
 }
