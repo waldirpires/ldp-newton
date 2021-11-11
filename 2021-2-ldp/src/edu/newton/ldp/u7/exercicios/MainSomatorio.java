13d56787671d509518eda761526bc82cbfce0b9f
Waldir R. Pires Jr <w.pires.ext@planerio.de>
2021-11-11 09:16:53 -0300

    10/11: Ex1: Somatorio

--- 2021-2-ldp/src/edu/newton/ldp/u7/exercicios/MainSomatorio.java
+++ 2021-2-ldp/src/edu/newton/ldp/u7/exercicios/MainSomatorio.java
@@ -8,7 +8,31 @@ public class MainSomatorio {
 	// ex: R = N + (N-1) + (N-2) + (N-3) . . . + 1
 	//
 	// implementar utilizando recursão E iteração
-	public static void main(String[] args) {
+	
+	static int somatorioIterativo(int n) { // iteração, laço
+		var s = 0;
 		
+		for (int i = n; i >=0; i--) {
+			System.out.println(String.format("%d: \t s = %d + %d", i, s, i));
+			s = s + i;
+		}
+		
+		return s;
+	}
+	
+	static int somatorio(int n) { // recursao
+		// caso base
+		if (n == 1) {
+			return n;
+		}
+		
+		// passo recursivo
+		return n + somatorio(n-1);
+	}
+	
+	public static void main(String[] args) {
+		int n = 6;
+		System.out.println("Somatório de "+ n + " (recursivo): " + somatorio(n));
+		System.out.println("Somatório de "+ n + " (iterativo): " + somatorioIterativo(n));
 	}
 }
